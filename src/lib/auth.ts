444c615bd75733f82350a6ba78615c14eba82c2b
shinaps <ryuta@shinaps.jp>
2025-07-31 02:13:16 +0900

    resolve issue

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,15 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import {drizzle} from "drizzle-orm/libsql";
 import {env} from "@/lib/env";
+import { createClient } from '@libsql/client/web';
+import { drizzle } from 'drizzle-orm/libsql';
 
-
-const db = drizzle({ connection: {
+const client = createClient({
   url: env.TURSO_DATABASE_URL,
   authToken: env.TURSO_AUTH_TOKEN
-}});
+});
+
+const db = drizzle(client);
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
