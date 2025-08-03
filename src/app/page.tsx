d3dc0886a209271fb219ae8cbf36c9c43ae38e96
shinaps <ryuta@shinaps.jp>
2025-08-03 14:21:21 +0900

    chore: setup and run biome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 'use client'
 
-import {authClient} from "@/lib/auth-client";
 import { useRouter } from 'next/navigation'
+import { authClient } from '@/lib/auth-client'
 
 export default function Home() {
-  const {data, isPending} = authClient.useSession()
-const router = useRouter()
-  if (isPending) {
-    return  <div>Loading...</div>;
-  }
+	const { data, isPending } = authClient.useSession()
+	const router = useRouter()
+	if (isPending) {
+		return <div>Loading...</div>
+	}
 
-  if (!data) {
-    router.push('/sign-up');
-    return
-  }
+	if (!data) {
+		router.push('/sign-up')
+		return
+	}
 
-  return (
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-2xl font-bold">Welcome, {data.user.email}!</h1>
-        <p className="mt-4">You are logged in.</p>
-      </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen">
+			<h1 className="text-2xl font-bold">Welcome, {data.user.email}!</h1>
+			<p className="mt-4">You are logged in.</p>
+		</div>
+	)
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,23 +1,23 @@
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import {env} from "@/lib/env";
-import { createClient } from '@libsql/client/web';
-import { drizzle } from 'drizzle-orm/libsql';
-import * as schema from "@/drizzle/schemas";
+import { createClient } from '@libsql/client/web'
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { drizzle } from 'drizzle-orm/libsql'
+import * as schema from '@/drizzle/schemas'
+import { env } from '@/lib/env'
 
 const client = createClient({
-  url: env.TURSO_DATABASE_URL,
-  authToken: env.TURSO_AUTH_TOKEN
-});
+	url: env.TURSO_DATABASE_URL,
+	authToken: env.TURSO_AUTH_TOKEN,
+})
 
-const db = drizzle(client, {schema});
+const db = drizzle(client, { schema })
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "sqlite",
-    }),
-  emailAndPassword: {
-    enabled: true,
-  },
-  trustedOrigins: ["http://localhost:3000","http://localhost:8787", "https://thisis.ooo/"],
-});
+	database: drizzleAdapter(db, {
+		provider: 'sqlite',
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	trustedOrigins: ['http://localhost:8787', 'https://thisis.ooo/'],
+})
