2b9a8f51f4c4688b52c13d9bb5949ad82ac7cd9f
shinaps <ryuta@shinaps.jp>
2025-08-19 05:10:59 +0900

    Update incremental cache configuration to use regional cache with long-lived mode

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,10 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare'
 import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache'
+import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache'
 
 export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache,
+  incrementalCache: withRegionalCache(r2IncrementalCache, {
+    mode: 'long-lived',
+    shouldLazilyUpdateOnCacheHit: true,
+  }),
 })
