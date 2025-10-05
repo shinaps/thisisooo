453f9a6ad2b24318612adc63d6adfd54659f417a
shinaps <ryuta@shinaps.jp>
2025-10-06 01:04:16 +0900

    feat: Add loading skeleton components for various sections

--- src/app/articles/[articleId]/loading.tsx
+++ src/app/articles/[articleId]/loading.tsx
@@ -1,28 +1,37 @@
 import { Skeleton } from '@/components/ui/skeleton'
 
 export default function ArticleLoading() {
-	return (
-		<div className="flex flex-col px-4 py-8 gap-y-8 max-w-3xl mx-auto">
-			{/* Article Header */}
-			<div className="flex flex-col gap-y-4">
-				<Skeleton className="h-8 w-3/4" />
-				<div className="flex gap-x-4">
-					<Skeleton className="h-4 w-24" />
-					<Skeleton className="h-4 w-32" />
-				</div>
-			</div>
+  return (
+    <div className="flex flex-col grow px-4 py-8 gap-y-8">
+      {/* Article Header */}
+      <div className="flex flex-col gap-y-4">
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <div className="flex gap-x-4 justify-between">
+          <Skeleton className="h-4 w-24" />
+          <Skeleton className="h-4 w-32" />
+        </div>
+      </div>
 
-			{/* Article Content */}
-			<div className="flex flex-col gap-y-4">
-				<Skeleton className="h-4 w-full" />
-				<Skeleton className="h-4 w-full" />
-				<Skeleton className="h-4 w-5/6" />
-				<Skeleton className="h-4 w-full" />
-				<Skeleton className="h-4 w-4/5" />
-				<Skeleton className="h-4 w-full" />
-				<Skeleton className="h-4 w-full" />
-				<Skeleton className="h-4 w-3/4" />
-			</div>
-		</div>
-	)
+      {/* Article Content */}
+      <div className="flex flex-col gap-y-4">
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-5/6" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-4/5" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-3/4" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-5/6" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-4/5" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-3/4" />
+      </div>
+    </div>
+  )
 }
--- src/app/profile/loading.tsx
+++ src/app/profile/loading.tsx
@@ -1,31 +1,24 @@
 import { Skeleton } from '@/components/ui/skeleton'
 
 export default function ProfileLoading() {
-	return (
-		<div className="flex flex-col px-4 py-8 gap-y-8 max-w-md mx-auto">
-			{/* Avatar */}
-			<div className="flex justify-center">
-				<Skeleton className="h-24 w-24 rounded-full" />
-			</div>
+  return (
+    <div className="flex flex-col px-4 py-8 gap-y-8">
+      <div className="flex flex-col gap-y-4">
+        <div className="flex flex-col gap-y-2">
+          <Skeleton className="h-4 w-20" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+        <div className="flex flex-col gap-y-2">
+          <Skeleton className="h-4 w-24" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+      </div>
 
-			{/* Form Fields */}
-			<div className="flex flex-col gap-y-4">
-				<div className="flex flex-col gap-y-2">
-					<Skeleton className="h-4 w-20" />
-					<Skeleton className="h-10 w-full" />
-				</div>
-				<div className="flex flex-col gap-y-2">
-					<Skeleton className="h-4 w-24" />
-					<Skeleton className="h-10 w-full" />
-				</div>
-			</div>
-
-			{/* Action Buttons */}
-			<div className="flex flex-col gap-y-2">
-				<Skeleton className="h-10 w-full" />
-				<Skeleton className="h-10 w-full" />
-				<Skeleton className="h-10 w-full" />
-			</div>
-		</div>
-	)
+      <div className="flex flex-col gap-y-2">
+        <Skeleton className="h-10 w-full" />
+        <Skeleton className="h-10 w-full" />
+        <Skeleton className="h-10 w-full" />
+      </div>
+    </div>
+  )
 }
